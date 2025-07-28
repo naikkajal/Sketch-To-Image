442f9d548fb79798cb833a3c46f842fdc5d3cb7c
Kajal Naik <naikkajal0603@gmail.com>
2025-07-28 23:56:26 +0530

    added springboot template

---
+++ Backend/sketchToImage/src/main/java/com/project/sketchToImage/SketchToImageApplication.java
@@ -0,0 +1,13 @@
+package com.project.sketchToImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchToImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchToImageApplication.class, args);
+	}
+
+}
---
+++ Backend/sketchToImage/src/test/java/com/project/sketchToImage/SketchToImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sketchToImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchToImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
